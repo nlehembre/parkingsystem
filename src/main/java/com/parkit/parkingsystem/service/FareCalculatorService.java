3485616754cdeea4be01000a29d56b819fc0b053
LEHEMBRE Nathalie <nathalie.lehembre@allianz.fr>
2020-12-11 18:52:11 +0100

    TODO#1

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,19 +10,21 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        // NL : conversion in mns
+
+        long durationMn = (outHour - inHour) / (60 * 1000) ;
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(durationMn * Fare.CAR_RATE_PER_HOUR / 60);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(durationMn * Fare.BIKE_RATE_PER_HOUR / 60 );
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
